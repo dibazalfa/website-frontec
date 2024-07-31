269b15845079b2dd487f504d576a14b9ddae37b9
dibazalfa <dibazalfa@gmail.com>
2024-07-31 11:33:43 +0700

    minor update: pulley slide page

--- frontend-frontec/src/views/SpareParts/BucketElevatorComponents/PulleySlideLagging/PulleySlideLagging.vue
+++ frontend-frontec/src/views/SpareParts/BucketElevatorComponents/PulleySlideLagging/PulleySlideLagging.vue
@@ -164,13 +164,11 @@
                 Elevator Bucket
               </p>
             </router-link>
-            <router-link to="/pulley-slide-lagging">
               <p
                 class="text-[18px] px-4 py-8 border-b border-black bg-gradient-to-r from-[#286F9B] to-[#96c4df] font-bold text-white"
               >
                 Pulley Slide Lagging
               </p>
-            </router-link>
           </div>
           <p class="text-[20px] mx-auto mt-10">In Partnership with:</p>
           <img :src="sanwei" alt="" class="w-64 mx-auto mt-2" />
