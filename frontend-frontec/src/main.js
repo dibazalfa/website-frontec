7b30a7e61e0daaffcdf7b1e9f081e9f4265e125e
aqilaqsa <aqilakill@gmail.com>
2024-08-01 10:11:09 +0700

    fixing UX and add more tables

--- frontend-frontec/src/main.js
+++ frontend-frontec/src/main.js
@@ -6,6 +6,16 @@ import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faCircleArrowLeft);
+
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
+
 
 const app = createApp(App)
 // const pinia = createPinia()
