614cd4454fe74a69592a1f9cece68d287fa2ace6
aqilaqsa <aqilakill@gmail.com>
2024-08-08 09:17:59 +0700

    improve UX

--- frontend-frontec/src/views/SpareParts/BeltConveyorComponents/Patterned.vue
+++ frontend-frontec/src/views/SpareParts/BeltConveyorComponents/Patterned.vue
@@ -24,43 +24,77 @@
             Patterned Conveyor Belt
           </p>
         </div>
-        <div class="flex flex-col md:flex-row mt-12">
-          <img :src="product" alt="" class="object-contain" />
-          <div
-            class="flex flex-col text-justify mt-6 md:mt-0 md:pl-10 text-[16px] md:text-[18px]"
-          >
-            <p
-              class="bg-black text-white w-36 px-6 py-3 rounded-full text-center font-bold uppercase shadow-lg"
-            >
-              Features
-            </p>
-            <ul class="list-disc mt-8 ml-6">
-              <li>
-                Suitable to convey materials such as powder, granules as well as
-                packaged materials at angleless than 40 degrees.
-              </li>
-              <li>
-                The carcass can be made from Polyester EP, Nylon NN, Cotton CC,
-                and Polyester Cotton TC;
-              </li>
-              <li>
-                Patterned Belt includes various patterns such as “Cross”, “V”,
-                “Doted”, “Crescent”, etc.
-              </li>
-              <li>
-                Excluding the pattern measurements, the overall belt
-                specifications and rubber cover properties of the patterned
-                conveyor belt remain the same as the flat surface conveyor belt
-                under the condition of the same carcass and rubber cover
-                materials.
-              </li>
-              <li>
-                The belts include general use, oil-resistant, acid-alkali
-                resistant, cold-resistant, static conductive, fire retardant,
-                wear-resistant and heat resistant types etc.
-              </li>
-            </ul>
+        <div class="flex flex-col items-center mt-12">
+          <img
+            :src="highlightedProduct"
+            alt="Highlighted Product"
+            class="object-contain mb-6 frame-border"
+          />
+          <div class="text-center mt-0 mb-4">
+            <p class="roboto-light text-gray-600">Click one of the images below to highlight</p>
           </div>
+          <div class="flex space-x-4 flex-wrap justify-center">
+            <img
+              :src="product1"
+              alt="Product 1"
+              class="object-contain border-4 border-white cursor-pointer w-24 h-24 thumbnail-border hover:scale-105 transform transition-transform duration-300"
+              @click="highlightProduct('product1')"
+            />
+            <img
+              :src="product2"
+              alt="Product 2"
+              class="object-contain border-4 border-white cursor-pointer w-24 h-24 thumbnail-border hover:scale-105 transform transition-transform duration-300"
+              @click="highlightProduct('product2')"
+            />
+            <img
+              :src="product3"
+              alt="Product 3"
+              class="object-contain border-4 border-white cursor-pointer w-24 h-24 thumbnail-border hover:scale-105 transform transition-transform duration-300"
+              @click="highlightProduct('product3')"
+            />
+            <img
+              :src="product4"
+              alt="Product 4"
+              class="object-contain border-4 border-white cursor-pointer w-24 h-24 thumbnail-border hover:scale-105 transform transition-transform duration-300"
+              @click="highlightProduct('product4')"
+            />
+          </div>
+        </div>
+        <br />
+        <div
+          class="flex flex-col text-justify mt-6 md:mt-0 md:pl-10 text-[16px] md:text-[18px]"
+        >
+          <p
+            class="bg-black text-white w-36 px-6 py-3 rounded-full text-center font-bold uppercase shadow-lg mx-auto md:mx-0"
+          >
+            Features
+          </p>
+          <ul class="list-disc mt-8 ml-6">
+            <li>
+              Suitable to convey materials such as powder, granules as well as
+              packaged materials at angleless than 40 degrees.
+            </li>
+            <li>
+              The carcass can be made from Polyester EP, Nylon NN, Cotton CC,
+              and Polyester Cotton TC;
+            </li>
+            <li>
+              Patterned Belt includes various patterns such as “Cross”, “V”,
+              “Doted”, “Crescent”, etc.
+            </li>
+            <li>
+              Excluding the pattern measurements, the overall belt
+              specifications and rubber cover properties of the patterned
+              conveyor belt remain the same as the flat surface conveyor belt
+              under the condition of the same carcass and rubber cover
+              materials.
+            </li>
+            <li>
+              The belts include general use, oil-resistant, acid-alkali
+              resistant, cold-resistant, static conductive, fire retardant,
+              wear-resistant and heat resistant types etc.
+            </li>
+          </ul>
         </div>
       </section>
 
@@ -71,10 +105,10 @@
           <div
             class="mt-10 bg-white rounded-lg shadow-md w-64 mx-auto text-center border border-black border-1"
           >
-              <p class="text-[20px] font-bold p-4 border-b border-black">
-                Belt Conveyor Components
-              </p>
-            <router-link to="flat-surface-conveyor">
+            <p class="text-[20px] font-bold p-4 border-b border-black">
+              Belt Conveyor Components
+            </p>
+            <router-link to="/flat-surface-conveyor">
               <p
                 class="text-[18px] px-4 py-8 border-b border-black hover:bg-gray-200"
               >
@@ -110,8 +144,10 @@ import AppHeader from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import background from "@/assets/img/spare-parts/bgSpareParts.png";
 import sanwei from "@/assets/img/spare-parts/sanwei.png";
-
-import product from "@/assets/img/spare-parts/patterned.png";
+import product1 from "@/assets/img/spare-parts/patterned-conveyor-belt1.png";
+import product2 from "@/assets/img/spare-parts/patterned-conveyor-belt2.png";
+import product3 from "@/assets/img/spare-parts/patterned-conveyor-belt3.png";
+import product4 from "@/assets/img/spare-parts/patterned-conveyor-belt4.png";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
@@ -125,8 +161,17 @@ export default {
     return {
       background,
       sanwei,
-      product,
+      product1,
+      product2,
+      product3,
+      product4,
+      highlightedProduct: product1,
     };
+  },
+  methods: {
+    highlightProduct(product) {
+      this.highlightedProduct = this[product];
+    },
   },
 };
 </script>
@@ -138,10 +183,6 @@ export default {
 
 .text-shadow-md {
   text-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
-}
-
-.bg-gray-100 {
-  background-color: #f7fafc;
 }
 
 .h-full {
@@ -156,7 +197,6 @@ export default {
   padding: 1rem;
 }
 
-/* Styling for the main content and sidebar */
 .main {
   flex: 1;
   overflow-y: hidden;
@@ -165,7 +205,7 @@ export default {
 }
 
 section {
-  overflow-y: auto; /* Enables scrolling on the main content */
+  overflow-y: auto;
 }
 
 .sidebar {
@@ -184,16 +224,36 @@ section {
   .sidebar {
     width: 100%;
     height: auto;
-    position: relative; /* Disable sticky on mobile if needed */
+    position: relative;
   }
 }
 
 .header-color {
   background-color: #286f9b;
-  color: white; /* Optional: to make the text color white */
+  color: white;
 }
 
 .center-text {
   text-align: center;
+}
+
+.frame-border {
+  border: 8px solid #ffffff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.thumbnail-border {
+  border: 4px solid #ffffff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  width: 100px;
+  height: 100px;
+}
+
+.bg-gradient-to-r {
+  background: linear-gradient(to right, #286f9b, #96c4df);
+}
+
+.hover\:scale-125:hover {
+  transform: scale(1.25);
 }
 </style>
