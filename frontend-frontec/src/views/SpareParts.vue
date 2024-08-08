9896036c1766725d10c0fd8075e25b7bf8bb2c2a
dibazalfa <dibazalfa@gmail.com>
2024-08-08 08:31:48 +0700

    update: spare parts

--- frontend-frontec/src/views/SpareParts.vue
+++ frontend-frontec/src/views/SpareParts.vue
@@ -6,54 +6,56 @@
       :isScrolled="isScrolled"
     />
     <main>
-      <section class="bg-white flex flex-col p-4">
-        <p class="mx-auto mt-10 font-[Inter-ExtraBold,Helvetica]">
-          In partnership with:
-        </p>
-        <img :src="sanwei" alt="" class="mx-auto mt-10" />
-      </section>
-      <section class="flex justify-center items-center min-h-[75vh]">
-        <div
-          class="w-2/5 h-96 p-4 flex flex-col md:flex-row items-center justify-center rounded-tl-3xl rounded-bl-3xl border-2 border-slate-300 border-r-0"
-          :style="{
-            background: `linear-gradient(to right, white, ${
-              isHovered ? currentTab.color : originalColor
-            })`,
-            transition: 'background 0.5s ease-in-out',
-          }"
-        >
-          <img
-            :src="currentTab.image"
-            alt="Placeholder Image"
-            class="tab-image mb-0 md:mb-4"
-          />
-          <div class="flex flex-col pl-3 md:pl-6">
-            <router-link :to="currentTab.route">
-              <p
-                class="uppercase font-bold text-3xl hover:underline hover:scale-105 transform transition-transform duration-300"
-                @mouseover="isHovered = true"
-                @mouseleave="isHovered = false"
-              >
-                {{ currentTab.title }}
-              </p>
-              <p>{{ currentTab.description }}</p>
-            </router-link>
-          </div>
-        </div>
-        <div class="flex flex-col">
-          <p
-            v-for="(tab, index) in tabs"
-            :key="index"
-            class="p-4 h-16 cursor-pointer rounded-tr-xl rounded-br-xl transition-transform duration-300 ease-in-out"
-            :class="{
-              'translate-x-2 shadow-lg': selectedTabIndex === index,
-              'bg-opacity-50': selectedTabIndex !== index,
+      <div class="flex flex-col md:flex-row">
+        <section class="bg-white flex flex-col p-4 w-1/3 mt-10 ml-12">
+          <p class="mx-auto mt-10 font-[Inter-ExtraBold,Helvetica]">
+            In partnership with:
+          </p>
+          <img :src="sanwei" alt="" class="mx-auto mt-10" />
+        </section>
+        <section class="flex justify-center items-center min-h-[75vh] w-2/3">
+          <div
+            class="w-7/12 h-96 p-4 flex flex-col md:flex-row items-center justify-center rounded-tl-3xl rounded-bl-3xl border-2 border-slate-300 border-r-0"
+            :style="{
+              background: `linear-gradient(to right, white, ${
+                isHovered ? currentTab.color : originalColor
+              })`,
+              transition: 'background 0.5s ease-in-out',
             }"
-            :style="{ backgroundColor: tab.color }"
-            @click="selectTab(index)"
-          ></p>
-        </div>
-      </section>
+          >
+            <img
+              :src="currentTab.image"
+              alt="Placeholder Image"
+              class="tab-image mb-0 md:mb-4"
+            />
+            <div class="flex flex-col pl-3 md:pl-6">
+              <router-link :to="currentTab.route">
+                <p
+                  class="uppercase font-bold text-xl md:text-3xl hover:underline hover:scale-105 transform transition-transform duration-300"
+                  @mouseover="isHovered = true"
+                  @mouseleave="isHovered = false"
+                >
+                  {{ currentTab.title }}
+                </p>
+                <p>{{ currentTab.description }}</p>
+              </router-link>
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <p
+              v-for="(tab, index) in tabs"
+              :key="index"
+              class="p-4 h-16 cursor-pointer rounded-tr-xl rounded-br-xl transition-transform duration-300 ease-in-out"
+              :class="{
+                'translate-x-2 shadow-lg': selectedTabIndex === index,
+                'bg-opacity-50': selectedTabIndex !== index,
+              }"
+              :style="{ backgroundColor: tab.color }"
+              @click="selectTab(index)"
+            ></p>
+          </div>
+        </section>
+      </div>
       <Footer></Footer>
     </main>
   </div>
